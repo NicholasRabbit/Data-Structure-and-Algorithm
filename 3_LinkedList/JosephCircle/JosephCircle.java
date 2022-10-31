f1c9b0c3523cbc0a6eb5bdae4e27201296e8f479
plms <askdell@sina.com>
2022-10-31 22:35:29 +0800

    Josephu Circle pop

--- 3_LinkedList/JosephCircle/JosephCircle.java
+++ 3_LinkedList/JosephCircle/JosephCircle.java
@@ -1,6 +1,6 @@
 
 /*
-约瑟夫环
+约瑟夫环，实际是一个单向链表环。
 */
 public class JosephCircle{
 	
@@ -18,7 +18,7 @@ public class JosephCircle{
 		//(2)创建一个辅助指针，帮助遍历添加，这个指针始终指向最后添加的一个节点，这样就方便在它后面进行添加了。
 		NodeDemo curNode = null;
 		for(int i = 1; i < num; i++){  //节点下标从1开始，方便对应号码。
-			//首先创建一个心得节点，准备添加
+			//首先创建一个新的节点，准备添加
 			NodeDemo newNode = new NodeDemo(i);
 			//(3)这里先判断i==1，是为了先给第一个节点first赋值，方便以后指向它，如果不加每次循环都会给first赋值，显然不对。
 			if(i == 1){
@@ -55,11 +55,66 @@ public class JosephCircle{
 
 	}
 
+	/*
+	* 三，出圈操作，即从第n个节点开始数，再数到m个出圈
+	* int totalNum : 约瑟夫环中的节点数
+	* 
+	*/
+	public NodeDemo pop(int startNo,int countNo,int totalNum){
+		//1）首先校验参数的合法性，如果约瑟夫环的第一个节点为空则它不存在
+		//   开始数小于1说明也不可能，countNo == totalNum老师课堂代码校验了，
+		//   其实可以不校验，因为是环形的，数到最后一个还可以继续从第一个数
+		if(first == null || startNo < 1 || countNo == totalNum){
+			System.out.println("输入数据不合理");
+		}
+		/*
+		* 2）设置一个变量endNode，指向环的最后一个节点，后面让节点出出圈时会用到 
+		*/
+		NodeDemo endNode = first;  //先让它指向第一个节点，开始遍历
+		while(true){
+			if(endNode.next == first){
+				break;
+			}
+			endNode = endNode.next;
+		}
+		/**
+		* 3）让first和endNode移动到开始的节点，因为first最开始就指向第一个节点，所以从第n个节点开始，就移动n-1个位置。
+		*    endNode同理。
+		*/
+		for(int i = 0; i < startNo -1; i++){
+			first = first.next;
+			endNode = endNode.next;
+		}
 
+		/*
+		* 4）进行出圈操作，这里执行while循环，按照规定直到剩一个节点为止。
+		*/
+		while(true){
+			if(endNode == first){  //最后一个节点就是第一个节点，说明最开始只添加了一个节点。
+				break;
+			}
+			//向后数m个节点出圈，就从当前节点后移m-1个位置
+			for(int j = 0; j < countNo - 1; j++){
+				first = first.next;
+				endNode = endNode.next;
+			}
+			//上面for循环结束后，first指向要出圈的节点。
+			//下面是进行出圈操作
+			first = first.next;  //节点后移一位，指向新的头节点
+			endNode = first;     //最后一个节点的next指向新的头节点，出圈的就没有引用指向了，即为出圈。
+		}
+		//5）最后first就指向圈中唯一剩余的一个节点
+		System.out.println("按规定令节点出圈后，剩余最后一个==>" + first.id);
+		System.out.println("first.hashCode()==>" + first.hashCode() + "\t\n" + "endNode==>" + endNode.hashCode());
 
+		return first;		
+
+	}
 
 }
 
+
+//节点
 class NodeDemo {
 	int id;
 	NodeDemo next;   //这里属性公开，不用set,get方法，代码简洁
--- 3_LinkedList/JosephCircle/JosephCircleTest.java
+++ 3_LinkedList/JosephCircle/JosephCircleTest.java
@@ -6,10 +6,16 @@
 public class JosephCircleTest{
 
 	public static void main(String[] args){
-		
+		//(1)简单测试
 		JosephCircle jc = new JosephCircle();
 		jc.create(5);
 		jc.showList(jc);
+		System.out.println("============分隔符===============");
+		//(2)进行出圈操作
+		JosephCircle jc2 = new JosephCircle();
+		jc2.create(100);
+		jc2.showList(jc2);
+		jc2.pop(1,5,100);
 		
 	}
 }
