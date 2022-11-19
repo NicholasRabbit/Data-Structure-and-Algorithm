3c03c739dd5ad5d64d5cde7406c97006ca55e550
plms <askdell@sina.com>
2022-11-19 11:10:33 +0800

    修复栈计算器多位数计算错误

--- 4_Stack/2_Calculator/CalStackTest.java
+++ 4_Stack/2_Calculator/CalStackTest.java
@@ -19,21 +19,43 @@ public class CalStackTest {
 	//计算器方法
 	public static void calculate(){
 	
-		String exp = "7+3*5-2";  //先写死表达式		
+		//String exp = "6+3*5-2"
+		String exp = "60+3*5/15-2";  //先写死表达式		
 		char[] charArr = exp.toCharArray();
+		
 		//创建两个栈，一个数栈，一个运算符栈
 		CalculatorStack numberStack = new CalculatorStack(5);  //初始化容量为5，注意计算位数多时适当加大栈的容量
 		CalculatorStack operStack = new CalculatorStack(5);  
+		
 		//遍历表达式
+		String concatNumber = "";  //拼接多位数用
 		for(int i = 0; i < charArr.length; i++){
 			char ch = charArr[i];
 			//判断是数还是操作符
 			boolean isNumber = numberStack.isNumber(ch);
 			
 			if(isNumber){
-				//是数字则直接入栈
-				//因为char类型的0-9实际用int的48-57表示，而运算要用int的所以这里转化为int类型，方便直接运算
-				numberStack.push(ch - 48); 
+				
+				/*
+				是数字则直接入栈
+				因为char类型的0-9实际用int的48-57表示，而运算要用int的所以这里转化为int类型，方便直接运算
+				*/
+				//numberStack.push(ch - 48);   //这里判断有个问题，两位数的数字无法一次入栈
+				
+				/*
+				下面进行判断，避免上述错误
+				nextIndex:指向当前扫描到字符的下一位，用于判断是否为操作符，不要用i++会影响整个循环
+				concatNumber: 进行拼接多位数
+				*/
+				concatNumber += ch;  
+				int nextIndex = i + 1;  
+				//如果当前数的下标是最后一位(不加此判断会导致下标越界)，或者下一位是符号则把当前拼接的数入栈，否则继续循环拼接
+				if(i == charArr.length - 1 || operStack.isOper(charArr[nextIndex])){
+					numberStack.push(Integer.parseInt(concatNumber));  //这里用parseInt()方法，直接把字符串转成同样的int类型了，不需要再减48了。
+					concatNumber = "";  //注意：这里入栈后把拼接的字符串初始化
+				}
+
+
 			}else{
 				//操作符栈内如果是空，或者扫描到的操作符优先级大于栈顶的，则直接入栈
 				if(operStack.isEmpty() || operStack.checkPriority(ch) > operStack.checkPriority((char)operStack.getTop())){
